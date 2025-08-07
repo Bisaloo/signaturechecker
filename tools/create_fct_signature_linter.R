394c77f85fef75f4322ab0234c95598cfa1cc3b3
Hugo Gruson <10783929+Bisaloo@users.noreply.github.com>
2025-08-07 18:47:09 +0200

    Add code to recursively get formals from ellipses

--- tools/create_fct_signature_linter.R
+++ tools/create_fct_signature_linter.R
@@ -1,15 +1,20 @@
-create_fct_signature_linter <- function(fct, fct_name = deparse(substitute(fct))) {
+create_fct_signature_linter <- function(
+  fct,
+  fct_name = deparse(substitute(fct)),
+  formals_env = new.env()
+) {
 
-  # As per ?formals, the `args()` intermediate step is required for
-  formals <- methods::formalArgs(args(fct))
+  formals_env[["formals"]] <- NULL
+  formals_env[["depth"]] <- 0
+  success <- get_recursive_formals(fct)
 
-  if ("..." %in% formals) {
-    # Get
-    formals <- setdiff(formals, "...")
+  if (!success) {
     return(invisible(FALSE))
   }
 
-  if (length(formals) == 0) {
+  frmls <- setdiff(unique(formals_env[["formals"]]), "...")
+
+  if (length(frmls) == 0) {
     return(invisible(FALSE))
   }
 
@@ -32,4 +37,80 @@ create_fct_signature_linter <- function(fct, fct_name = deparse(substitute(fct))
   )
 
   invisible(TRUE)
+}
+
+get_recursive_formals <- function(fct) {
+  # As per ?formals, the `args()` intermediate step is required for primitives
+  formals_env[["formals"]] <<- c(formals_env[["formals"]], methods::formalArgs(args(fct)))
+  formals_env[["depth"]] <<- formals_env[["depth"]] + 1
+  if (formals_env[["depth"]] > 20) {
+    return(FALSE)
+  }
+
+  # FIXME: There is surely a better way to parse from the installed sources,
+  # rather than resaving the file, but I can't figure it out now
+  tmp <- tempfile(fileext = ".R")
+
+  def <- body(fct)
+
+  if (is.null(def) || any(grepl("UseMethod", def))) {
+    # We have encountered a Primitive. All hope is lost. Flush formals and abort.
+    formals_env[["formals"]] <- NULL
+    return(FALSE)
+  }
+
+  writeLines(
+    format(body(fct)),
+    tmp,
+    sep = "\n"
+  )
+
+  parsed_body <- parse(tmp) |>
+    xmlparsedata::xml_parse_data(pretty = TRUE) |>
+    xml2::as_xml_document()
+
+  hidden_ellipsis_fcts <- xml2::xml_find_first(
+    parsed_body,
+    "SYMBOL_FUNCTION_CALL[text() = '...length' or text() = '...names()']"
+  )
+  if (length(hidden_ellipsis_fcts) > 0) {
+    formals_env[["formals"]] <- NULL
+    return(FALSE)
+  }
+
+  next_fct_calls <- parsed_body |>
+    xml2::xml_find_all(
+      "//SYMBOL[text() = '...']
+        /parent::expr
+        /parent::expr
+        /expr[1]
+        /SYMBOL_FUNCTION_CALL
+      "
+    )
+
+  internal_calls <- xml2::xml_find_first(
+    next_fct_calls,
+    "//SYMBOL_FUNCTION_CALL[text() = '.Internal']"
+  )
+
+  if (any(!is.na(internal_calls))) {
+    # This might be overkill. If the function calling Internal() doesn't have
+    # ..., we may still be able to extract args
+    formals_env[["formals"]] <- NULL
+    return(FALSE)
+  }
+
+  next_fcts <- next_fct_calls |>
+    xml2::xml_text() |>
+    unique()
+
+  if ("FUN" %in% next_fcts) {
+    # Conventionally, FUN is a user-defined function
+    formals_env[["formals"]] <- NULL
+    return(FALSE)
+  }
+
+  success <- purrr::map_lgl(next_fcts, get_recursive_formals)
+
+  return(all(success))
 }
--- tools/create_linters.R
+++ tools/create_linters.R
@@ -3,14 +3,26 @@ source(here::here("tools", "create_fct_signature_linter.R"))
 base_fcts <- grepv("^[[:alpha:]_.]*$", ls(.BaseNamespaceEnv))
 base_fcts <- setdiff(
   base_fcts,
+  # symbols
   c("F", "T", "last.warning", "letters", "LETTERS", "month.abb", "month.name", "pi", "R.version", "R.version.string", "version")
+)
+base_fcts <- setdiff(
+  base_fcts,
+  c("library", "library.dynam") # FIXME: why doesn't this work?
+)
+base_fcts <- setdiff(
+  base_fcts,
+  "Negate" # uses f for an arbitrary function arg
 )
 
 has_linter <- logical(length(base_fcts))
+
+formals_env <- new.env()
+
 for (i in seq_along(base_fcts)) {
   fct_name <- base_fcts[i]
   fct <- rlang::as_function(fct_name)
-  has_linter[i] <- create_fct_signature_linter(fct, fct_name)
+  has_linter[i] <- create_fct_signature_linter(fct, fct_name, formals_env)
 }
 
 write.csv(
